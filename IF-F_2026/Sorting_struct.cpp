c72e7ebd5a27fb5f952c1aa59a408fd9c465d067
Morxidia <morxidia@gmail.com>
2026-05-04 10:17:36 +0700

    s

--- IF-F_2026/Sorting_struct.cpp
+++ IF-F_2026/Sorting_struct.cpp
@@ -33,7 +33,6 @@ int main(void)
     int sizeMahasiswa = 50;
     Mahasiswa *Array = GenRandUsers(sizeMahasiswa);
     printArray(Array, sizeMahasiswa);
-    //test this comment 
     printArray(Array, sizeMahasiswa);
     delete[] Array;
     return 0;
